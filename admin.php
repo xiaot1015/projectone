83d4e1617e0b6168ee2dd1a5ee268d2b31d3fc70
xiaotao <xiaotao@didichuxing.com>
2016-08-07 00:14:43 +0800

    test

--- admin.php
+++ admin.php
@@ -2,7 +2,7 @@
 // 定义ThinkPHP框架路徂
 define('THINK_PATH', './ThinkPHP/');
 //定义项目名称和路徂
-define('APP_NAME', 'Hongy');
+define('APP_NAME', 'ProjectOne');
 define('APP_PATH', './Admin/');
 define('APP_DEBUG', TRUE);
 define('FRONT', 'http://172.20.0.100/index.php/');
